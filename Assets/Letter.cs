ac43b0fb4aaa20ad8d7334351a3f6dd56ec9d41b
Igor Iwashita <oliveiraigorr@hotmail.com>
2022-02-27 18:56:11 -0300

    First commit

---
+++ Assets/Letter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter
+{
+    public string id { get; set; }
+    public char content { get; set; }
+    public bool isHidden { get; set; }
+
+
+}
