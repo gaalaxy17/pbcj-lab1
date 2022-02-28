7f3389eccc0bd1a7732c98b035b94be307a678fc
caio_cerano <caiocerano@gmail.com>
2022-02-28 01:15:32 -0300

    Testes com botões bugados

--- Assets/Scripts/ManageEnd.cs
+++ Assets/Scripts/ManageEnd.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class ManageButtons : MonoBehaviour
+public class ManageEnd : MonoBehaviour
 {
     public GameObject message;
     public GameObject lastHiddenWord; // Palavra selecionada
